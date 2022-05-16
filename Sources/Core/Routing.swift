2a6536513fa8071a3a08ee5a545c00583c993ef5
ueshun109 <ush109rock@gmail.com>
2022-05-16 09:44:49 +0900

    :sparkles: Add a module that the core functions of the app

---
+++ Sources/Core/Routing.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+/// 遷移先を列挙する振る舞いをもたせる`protocol`
+public protocol Route {}
+
+/// 遷移先のインスタンスを生成する振る舞いをもたせる`protocol`
+@MainActor
+public protocol Routing {
+  associatedtype _Route: Route
+  associatedtype View: SwiftUI.View
+
+  /// Routeに準拠したenumから次の遷移先の`View`のインスタンスを返す
+  /// - Parameter with : 遷移先が列挙されたenumの値
+  /// - Returns: 遷移先の`View`インスタンス
+  @ViewBuilder func view(for route: _Route) -> Self.View
+
+  /// ディープリンクから次の遷移先を表す`Route`インスタンスを返す
+  /// - Returns: 次の遷移先の`Route`
+  func route(from deeplink: URL) -> _Route?
+}
