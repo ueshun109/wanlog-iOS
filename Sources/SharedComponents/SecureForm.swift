614b2b5e3b78dc3bc8cc269b359b9adc5fdbddff
ueshun109 <ush109rock@gmail.com>
2022-05-22 12:40:33 +0900

    :sparkles: Add input form and secure form as shared components

--- Sources/SharedComponents/SecureForm.swift
+++ Sources/SharedComponents/SecureForm.swift
@@ -1,19 +1,16 @@
 import Styleguide
 import SwiftUI
 
-public struct InputForm: View {
+public struct SecureForm: View {
   @Binding private var text: String
   private let placeholder: String
-  private let keyboardType: UIKeyboardType
 
   public init(
     text: Binding<String>,
-    placeholder: String,
-    keyboardType: UIKeyboardType = .default
+    placeholder: String
   ) {
     self._text = text
     self.placeholder = placeholder
-    self.keyboardType = keyboardType
   }
 
   public var body: some View {
@@ -23,21 +20,12 @@ public struct InputForm: View {
         .foregroundColor(Color.Label.secondary)
         .fontWithLineHeight(font: .hiraginoSans(.caption1))
 
-      TextField(placeholder, text: $text)
+      SecureField(placeholder, text: $text)
         .foregroundColor(Color.Label.primary)
         .fontWithLineHeight(font: .hiraginoSans(.callout))
         .padding(Padding.xSmall)
         .background(Color.Fill.quaternary)
         .clipShape(RoundedRectangle(cornerRadius: 8))
-        .keyboardType(keyboardType)
-    }
-  }
-}
-
-struct InputForm_Previews: PreviewProvider {
-  static var previews: some View {
-    VStack {
-      InputForm(text: .constant(""), placeholder: "mail address")
     }
   }
 }
