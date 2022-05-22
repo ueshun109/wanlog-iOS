357f96ca599627475c1fd8c5b03a449b03a6ef8a
ueshun109 <ush109rock@gmail.com>
2022-05-22 12:37:24 +0900

    :+1: Add DataStore module.

---
+++ Sources/DataStore/UserState.swift
@@ -0,0 +1,30 @@
+import Combine
+import FirebaseClient
+import Foundation
+import SharedModels
+
+@MainActor
+public final class UserState: ObservableObject {
+  private let authenticator: Authenticator
+  @Published public private(set) var isSignIn = false
+  public private(set) var error = PassthroughSubject<LoadingError, Never>()
+
+  public init(authenticator: Authenticator) {
+    self.authenticator = authenticator
+  }
+
+  public func confirmAuthStatus() async {
+    isSignIn = await authenticator.isSignIn()
+  }
+
+  public func anonymousSignin() async {
+    do {
+      isSignIn = try await authenticator.anonymousSignin()
+    } catch let loadingError as LoadingError {
+      self.error.send(loadingError)
+    } catch {
+      let loadingError = LoadingError(errorDescription: error.localizedDescription)
+      self.error.send(loadingError)
+    }
+  }
+}
