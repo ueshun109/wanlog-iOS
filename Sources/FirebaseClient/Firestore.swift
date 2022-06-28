51fdb7e9a2b4cf7b275275db82032a2043db3a10
ueshun109 <ush109rock@gmail.com>
2022-06-28 19:12:52 +0900

    :+1: Update firestore extension

--- Sources/FirebaseClient/Firestore.swift
+++ Sources/FirebaseClient/Firestore.swift
@@ -5,53 +5,35 @@ import SharedModels
 
 public extension FirebaseFirestore.Firestore {
   func get<T>(_ reference: CollectionReference, type: T.Type) async throws -> [T]? where T: Decodable {
-    try await withCheckedThrowingContinuation { [weak self] continuation in
-      reference.getDocuments { querySnapshot, error in
-        if let error = error,
-           let loadingError = self?.handleError(error: error)?.toLoadingError {
-          logger.error(message: error)
-          continuation.resume(throwing: loadingError)
-          return
-        }
-        do {
-          let response: [T]? = try querySnapshot?.documents.compactMap { queryDocumentSnapshot in
-            return try queryDocumentSnapshot.data(as: type)
-          }
-          logger.info(message: "Succeeded in getting: \(String(describing: response))")
-          continuation.resume(returning: response)
-        } catch {
-          logger.error(message: error)
-          if let loadingError = self?.handleError(error: error)?.toLoadingError {
-            continuation.resume(throwing: loadingError)
-          } else {
-            continuation.resume(throwing: error)
-          }
-        }
+    do {
+      let querySnapshot = try await reference.getDocuments()
+      let response: [T]? = try querySnapshot.documents.compactMap { queryDocumentSnapshot in
+        return try queryDocumentSnapshot.data(as: type)
+      }
+      logger.info(message: "Succeeded in getting: \(String(describing: response))")
+      return response
+    } catch {
+      logger.error(message: error)
+      if let loadingError = handleError(error: error)?.toLoadingError {
+        throw loadingError
+      } else {
+        throw error
       }
     }
   }
 
   func get<T>(_ reference: DocumentReference, type: T.Type) async throws -> T? where T: Decodable {
-    try await withCheckedThrowingContinuation { [weak self] continuation in
-      reference.getDocument { snapshot, error in
-        if let error = error,
-           let loadingError = self?.handleError(error: error)?.toLoadingError {
-          logger.error(message: error)
-          continuation.resume(throwing: loadingError)
-          return
-        }
-        do {
-          let response: T? = try snapshot?.data(as: type)
-          logger.info(message: "Succeeded in getting: \(String(describing: response))")
-          continuation.resume(returning: response)
-        } catch {
-          logger.error(message: error)
-          if let loadingError = self?.handleError(error: error)?.toLoadingError {
-            continuation.resume(throwing: loadingError)
-          } else {
-            continuation.resume(throwing: error)
-          }
-        }
+    do {
+      let documentSnapshot = try await reference.getDocument()
+      let response: T? = try documentSnapshot.data(as: type)
+      logger.info(message: "Succeeded in getting: \(String(describing: response))")
+      return response
+    } catch {
+      logger.error(message: error)
+      if let loadingError = handleError(error: error)?.toLoadingError {
+        throw loadingError
+      } else {
+        throw error
       }
     }
   }
