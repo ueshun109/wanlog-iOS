3903ab612772d9a624dfc3b64aa518974bb1ec51
ueshun109 <ush109rock@gmail.com>
2022-06-28 19:50:54 +0900

    :+1: Add image

--- Sources/Styleguide/Images.swift
+++ Sources/Styleguide/Images.swift
@@ -1,7 +1,10 @@
 import SwiftUI
 
 public extension Image {
+  static let booksVertical = Self(systemName: "books.vertical")
+  static let calendar = Self(systemName: "calendar")
   static let chevronBack = Self(systemName: "chevron.backward")
   static let infoCircle = Self(systemName: "info.circle")
+  static let listDash = Self(systemName: "list.dash")
   static let person = Self(systemName: "person")
 }
