f8c264e071675ad9980be5ad333e35a8b51d0dfe
ueshun109 <ush109rock@gmail.com>
2022-07-07 00:04:56 +0900

    :bento: Add image

--- Sources/Styleguide/Images.swift
+++ Sources/Styleguide/Images.swift
@@ -2,8 +2,13 @@ import SwiftUI
 
 public extension Image {
   static let booksVertical = Self(systemName: "books.vertical")
+  static let clock = Self(systemName: "clock")
   static let calendar = Self(systemName: "calendar")
+  static let checkmark = Self(systemName: "checkmark")
+  static let checkmarkCircle = Self(systemName: "checkmark.circle")
+  static let checkmarkCircleFill = Self(systemName: "checkmark.circle.fill")
   static let chevronBack = Self(systemName: "chevron.backward")
+  static let circle = Self(systemName: "circle")
   static let infoCircle = Self(systemName: "info.circle")
   static let listDash = Self(systemName: "list.dash")
   static let person = Self(systemName: "person")
