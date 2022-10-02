163bbb56b1bccaa96ad7c882f6cf36cbde96cf8c
ueshun109 <ush109rock@gmail.com>
2022-10-02 20:46:01 +0900

    :+1: Add array which limited element count

---
+++ Sources/Core/LimitedArray.swift
@@ -0,0 +1,77 @@
+public struct LimitedArray<T: Equatable>: Equatable {
+  private let max: Int
+  private var elements: [T] = []
+
+  public init(_ max: Int) {
+    self.max = max
+  }
+
+  @discardableResult
+  public mutating func append(_ value: T) -> Bool {
+    if elements.count < max {
+      elements.append(value)
+      return true
+    } else {
+      logger.error(message: "Appendable element count is \(max)")
+      return false
+    }
+  }
+
+  @discardableResult
+  public mutating func remove(at index: Int) -> Bool {
+    guard index < elements.count else {
+      logger.error(message: "Index out of range.")
+      return false
+    }
+    elements.remove(at: index)
+    return true
+  }
+
+  @discardableResult
+  public mutating func replace(_ value: T, at index: Int) -> Bool {
+    guard index < elements.count else {
+      logger.error(message: "Index out of range.")
+      return false
+    }
+    elements[index] = value
+    return true
+  }
+}
+
+// MARK: - RandomAccessCollection
+
+extension LimitedArray: RandomAccessCollection {
+  public var startIndex: Int {
+    elements.startIndex
+  }
+
+  public var endIndex: Int {
+    elements.endIndex
+  }
+
+  public func index(after i: Int) -> Int {
+    elements.index(after: i)
+  }
+}
+
+extension LimitedArray: MutableCollection {
+  public subscript(position: Int) -> T {
+    get {
+      elements[position]
+    }
+    set {
+      replace(newValue, at: position)
+    }
+  }
+
+  public subscript(bounds: Range<Int>) -> ArraySlice<T> {
+    get {
+      elements[bounds]
+    }
+    set {
+      newValue.enumerated().forEach {
+        self[$0] = $1
+      }
+    }
+  }
+}
---
+++ Tests/CoreTests/LimitedArrayTest.swift
@@ -0,0 +1,75 @@
+import Core
+import XCTest
+
+class LimitedArrayTest: XCTestCase {
+  func testAppend() {
+    XCTContext.runActivity(named: "Append") { _ in
+      XCTContext.runActivity(named: "Up to the upper limit") { _ in
+        var array = LimitedArray<Int>(3)
+        XCTAssertTrue(array.append(1))
+        XCTAssertTrue(array.append(2))
+        XCTAssertTrue(array.append(3))
+        XCTAssertEqual(array.count, 3)
+      }
+
+      XCTContext.runActivity(named: "Exceeding upper limit") { _ in
+        var array = LimitedArray<Int>(3)
+        array.append(1)
+        array.append(2)
+        array.append(3)
+        XCTAssertFalse(array.append(4))
+        XCTAssertEqual(array.count, 3)
+        XCTAssertEqual(array.first, 1)
+        XCTAssertEqual(array[1], 2)
+        XCTAssertEqual(array.last, 3)
+      }
+    }
+  }
+
+  func testRemove() {
+    XCTContext.runActivity(named: "Replace") { _ in
+      XCTContext.runActivity(named: "In range") { _ in
+        var array = LimitedArray<Int>(3)
+        array.append(1)
+        array.append(2)
+        array.append(3)
+        array.remove(at: 2)
+        XCTAssertEqual(array.count, 2)
+      }
+
+      XCTContext.runActivity(named: "Out of range") { _ in
+        var array = LimitedArray<Int>(3)
+        array.append(1)
+        XCTAssertFalse(array.remove(at: 1))
+        array.append(2)
+        array.append(3)
+        XCTAssertFalse(array.remove(at: 3))
+        XCTAssertEqual(array.count, 3)
+      }
+    }
+  }
+
+  func testReplace() {
+    XCTContext.runActivity(named: "Replace") { _ in
+      XCTContext.runActivity(named: "In range") { _ in
+        var array = LimitedArray<Int>(3)
+        array.append(1)
+        array.append(2)
+        array.append(3)
+        array.replace(4, at: 1)
+        XCTAssertEqual(array[1], 4)
+        XCTAssertEqual(array.count, 3)
+      }
+
+      XCTContext.runActivity(named: "Out of range") { _ in
+        var array = LimitedArray<Int>(3)
+        array.append(1)
+        XCTAssertFalse(array.replace(1, at: 4))
+        array.append(2)
+        array.append(3)
+        XCTAssertFalse(array.replace(4, at: 5))
+        XCTAssertEqual(array.count, 3)
+      }
+    }
+  }
+}
